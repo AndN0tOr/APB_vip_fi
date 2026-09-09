3b69af65a1fbb635ba42ce6cd8670f7719095009
AndN0tOr <23521731@gm.uit.edu.vn>
2026-09-09 09:55:18 +0700

    include enum, def for global pkg

--- source/fpt_apb_global_pkg.svh
+++ source/fpt_apb_global_pkg.svh
@@ -1,8 +1,11 @@
 `ifndef FPT_APB_GLOBAL_PKG
 `define FPT_APB_GLOBAL_PKG
 
+`include "fpt_apb_enum_pkg.svh"
+
 package fpt_apb_global_pkg;
-    
+    `include "fpt_apb_def.svh"
+    import fpt_apb_enum_pkg::*;
 
 endpackage: fpt_apb_global_pkg
 
