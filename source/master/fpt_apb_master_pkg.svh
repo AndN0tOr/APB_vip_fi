85df8716745b792651e50c88dc7feb181ef6fdeb
AndN0tOr <23521731@gm.uit.edu.vn>
2026-09-09 09:55:43 +0700

    include for master pkg

---
+++ source/master/fpt_apb_master_pkg.svh
@@ -0,0 +1,17 @@
+`ifndef FPT_APB_MASTER_PKG_SVH
+`define FPT_APB_MASTER_PKG_SVH
+
+`include "uvm_macros.svh"
+`include "../fpt_apb_global_pkg.svh"
+
+package fpt_apb_master_pkg;
+    import uvm_pkg::*;
+    import fpt_apb_global_pkg::*;
+    import fpt_apb_enum_pkg::*;
+    `include "fpt_apb_master_seq_item.svh"
+endpackage
+
+
+
+
+`endif // FPT_APB_MASTER_PKG_SVH
