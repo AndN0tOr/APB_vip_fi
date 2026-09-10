29fa89cdae6737835113fc458cf143b0d91f1604
AndN0tOr <23521731@gm.uit.edu.vn>
2026-09-10 13:15:32 +0700

    change global package include.

--- source/fpt_apb_global_pkg.sv
+++ source/fpt_apb_global_pkg.sv
@@ -1,11 +1,9 @@
 `ifndef FPT_APB_GLOBAL_PKG
 `define FPT_APB_GLOBAL_PKG
 
-`include "fpt_apb_enum_pkg.sv"
 
 package fpt_apb_global_pkg;
     `include "fpt_apb_def.svh"
-    import fpt_apb_enum_pkg::*;
 
 endpackage: fpt_apb_global_pkg
 
