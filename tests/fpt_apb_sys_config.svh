22614f045c39a32567c5156ed8086bb57e341d83
AndN0tOr <23521731@gm.uit.edu.vn>
2026-09-16 12:57:49 +0700

    add include in env, encapsulate  malloc for arrays, create method, config variables in env

--- tests/fpt_apb_sys_config.svh
+++ tests/fpt_apb_sys_config.svh
@@ -1,6 +1,8 @@
 `ifndef FPT_APB_SYS_CONFIG_SVH
 `define FPT_APB_SYS_CONFIG_SVH
 
+`include "fpt_apb_mem_model_t.svh"
+
 class fpt_apb_sys_config extends uvm_object;
     `uvm_object_utils(fpt_apb_sys_config)
     
