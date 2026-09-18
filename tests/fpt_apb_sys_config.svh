ff0a0f1cbe6bd98c3f4a3094aa4880ba159c04f3
AndN0tOr <23521731@gm.uit.edu.vn>
2026-09-18 15:29:27 +0700

    upgrade sys config, and init example of config from the test.

--- tests/fpt_apb_sys_config.svh
+++ tests/fpt_apb_sys_config.svh
@@ -1,13 +1,21 @@
 `ifndef FPT_APB_SYS_CONFIG_SVH
 `define FPT_APB_SYS_CONFIG_SVH
 
+`include "fpt_apb_mem_model_t.svh"
+
 class fpt_apb_sys_config extends uvm_object;
     `uvm_object_utils(fpt_apb_sys_config)
     
     int unsigned fpt_clk_period;
 
     int unsigned fpt_master_numb = 1;
+    int unsigned fpt_master_priority[];
+
     int unsigned fpt_slave_numb = 1;
+    int unsigned fpt_mem_model_base_addr[];
+    int unsigned fpt_mem_model_addr_range[];
+
+    FPT_MEMORY_INIT_PATTERN_E fpt_mem_model_init_pattern[];
 
     int unsigned fpt_delay_pready_min;
     int unsigned fpt_delay_pready_max;
@@ -16,8 +24,6 @@ class fpt_apb_sys_config extends uvm_object;
     int unsigned fpt_delay_transfer_max;
 
     int unsigned fpt_pready_timeout;
-    int unsigned fpt_penable_timeout;
-
 
     extern function new(string name = "fpt_apb_sys_config");
 
@@ -26,5 +32,4 @@ endclass: fpt_apb_sys_config
 function fpt_apb_sys_config::new(string name = "fpt_apb_sys_config");
     super.new(name);
 endfunction: new
-
 `endif // FPT_APB_SYS_CONFIG_SVH
